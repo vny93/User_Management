957754d065a624e8e42c19631a94cbabaf61523d
Vu Ngan Yen <yen.vu@amitgroup.vn>
2023-07-24 13:37:45 +0700

    handle design app layout

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-
 import "./App.scss";
 import Header from "./components/Header";
+import Home from "./components/Home";
 import TableUsers from "./components/TableUsers";
 import Container from "react-bootstrap/Container";
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer, toast } from "react-toastify";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
-
-
   return (
     <>
       <div className="app-container">
-        <Header />
+      <Header />
         <Container>
-          <TableUsers />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/users" element={<TableUsers />} />
+          </Routes>
         </Container>
-
       </div>
       <ToastContainer
         position="top-right"
